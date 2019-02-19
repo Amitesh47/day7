d08a7f9b12313af0e381f646389b50fc769fdd9f
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-02-19 13:06:26 +0530

    sdbhasjk

---
+++ html&js/scripts/app.js
@@ -0,0 +1,28 @@
+/*var x = 10;
+var name = "amitesh";
+var users = ['john','kieth'];
+var clickme = function(){
+    alert("hello");
+    var testme = "xyz";
+};
+var y = true;
+
+console.log(typeof users);
+console.log(typeof x);
+console.log(typeof name);
+console.log(typeof fullname);
+console.log(typeof y);
+
+if(typeof(name) == "number"){
+    console.log("Success");
+}
+else{
+    console.log("Failure")
+}
+
+/*for(var i = 1; i<=10; i++){
+    console.log(i);
+}*/
+
+var buttons = document.getElementsByTagName('button');
+console.log(buttons);
---
+++ html&js/scripts/register.js
@@ -0,0 +1,11 @@
+var username  = document.getElementById('username');
+var pass = document.getElementById('password');
+var agr = document.getElementById('agree');
+var gen = document.getElementById('gender');
+
+var show = function(){
+    console.log(username.value);
+    console.log(pass.value);
+    console.log(agr.value);
+    console.log(gen.value);
+}
