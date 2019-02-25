531d011784c2ba64872700ab94333731e7b94f16
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-02-25 16:54:53 +0530

    add

---
+++ html&js/scripts/event.js
@@ -0,0 +1,30 @@
+(function(){
+    "use strict";
+    var key = document.getElementById('textual');
+    var eve = document.getElementById('buttons');
+    var num = document.getElementById('numeric');
+    eve.addEventListener('click', function(e){
+        console.log(e);
+        console.log(this);
+    });
+
+    key.addEventListener('keypress',function(e){
+        console.log(e);
+        console.log(this);
+        validateInput(e, /^[A-Za-z]+$/);
+    });
+
+    numeric.addEventListener('keypress',function(e){
+        validateInput(e, /^[0-9]*$/);
+    })
+
+    function validateInput(e,myregex){
+        if(myregex.test(e.key)){
+
+        }
+        else{
+            e.preventDefault();
+        }
+    }
+
+}) ();
---
+++ html&js/scripts/json.js
@@ -0,0 +1,235 @@
+var myData = [
+    {
+      "_id": "5c73b23e0daac75b72086988",
+      "index": 0,
+      "guid": "a8ebe569-7906-49e3-8225-fe0fb5daf7ce",
+      "isActive": true,
+      "balance": "$2,754.36",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Lyons Stein",
+      "gender": "male",
+      "company": "ECRAZE",
+      "email": "lyonsstein@ecraze.com",
+      "phone": "+1 (842) 446-3464",
+      "address": "412 Freeman Street, Windsor, Louisiana, 1832",
+      "about": "Non sunt esse ex est labore elit adipisicing dolor exercitation ea commodo elit labore. Do dolor reprehenderit magna reprehenderit officia occaecat laboris fugiat do. Deserunt elit eiusmod consectetur nostrud nostrud. Nostrud qui mollit aute et enim. Labore dolor et laboris elit cillum mollit tempor aliqua eiusmod ut. Laborum officia consequat excepteur eiusmod laboris nostrud labore ad.\r\n",
+      "registered": "2016-04-13T03:44:46 -06:-30",
+      "latitude": -2.71727,
+      "longitude": 150.096842,
+      "tags": [
+        "minim",
+        "laborum",
+        "qui",
+        "ad",
+        "tempor",
+        "labore",
+        "cillum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Linda Martinez"
+        },
+        {
+          "id": 1,
+          "name": "Dalton Horne"
+        },
+        {
+          "id": 2,
+          "name": "Bauer Albert"
+        }
+      ],
+      "greeting": "Hello, Lyons Stein! You have 3 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5c73b23ee05ee65345104714",
+      "index": 1,
+      "guid": "95661ac8-1a98-4bb3-8543-0086c403f5d5",
+      "isActive": true,
+      "balance": "$3,790.42",
+      "picture": "http://placehold.it/32x32",
+      "age": 38,
+      "eyeColor": "green",
+      "name": "Gallegos Pollard",
+      "gender": "male",
+      "company": "DAIDO",
+      "email": "gallegospollard@daido.com",
+      "phone": "+1 (882) 595-2961",
+      "address": "917 Bouck Court, Caspar, Michigan, 596",
+      "about": "Duis pariatur pariatur irure consequat non ipsum consectetur magna. Laborum nisi anim elit sit. Fugiat irure officia dolor anim pariatur culpa non laboris ad enim elit aliquip. Aliqua duis excepteur aute minim et nisi eiusmod. Ut anim consequat adipisicing do nulla quis tempor do velit. Aliquip laboris dolore qui eu.\r\n",
+      "registered": "2016-05-23T01:37:34 -06:-30",
+      "latitude": -80.601368,
+      "longitude": 15.971457,
+      "tags": [
+        "excepteur",
+        "do",
+        "amet",
+        "elit",
+        "deserunt",
+        "in",
+        "irure"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lane Bentley"
+        },
+        {
+          "id": 1,
+          "name": "Janie Gillespie"
+        },
+        {
+          "id": 2,
+          "name": "Brigitte Contreras"
+        }
+      ],
+      "greeting": "Hello, Gallegos Pollard! You have 3 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5c73b23ea6fab68aecd425a5",
+      "index": 2,
+      "guid": "3077141b-727b-4214-9451-b79af6c3982c",
+      "isActive": false,
+      "balance": "$3,261.15",
+      "picture": "http://placehold.it/32x32",
+      "age": 34,
+      "eyeColor": "brown",
+      "name": "Fischer Horton",
+      "gender": "male",
+      "company": "ZOLARITY",
+      "email": "fischerhorton@zolarity.com",
+      "phone": "+1 (965) 406-2767",
+      "address": "200 Danforth Street, Farmington, Idaho, 2697",
+      "about": "Dolore quis cillum elit non. Pariatur eiusmod nisi aliqua irure nisi et incididunt cupidatat occaecat dolore. Consequat deserunt duis minim excepteur magna ea veniam. Sit incididunt reprehenderit pariatur eu consequat ut irure dolore labore aliquip ullamco. Incididunt amet adipisicing est esse cillum pariatur ea do enim culpa anim minim. Fugiat id nisi adipisicing sint deserunt id occaecat est id reprehenderit amet duis. Consectetur nostrud aliquip ea commodo fugiat aute reprehenderit officia irure laboris laboris dolore et reprehenderit.\r\n",
+      "registered": "2016-05-31T01:33:09 -06:-30",
+      "latitude": 44.497441,
+      "longitude": 71.098043,
+      "tags": [
+        "anim",
+        "id",
+        "nostrud",
+        "enim",
+        "et",
+        "laboris",
+        "duis"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Rhodes Johnson"
+        },
+        {
+          "id": 1,
+          "name": "Marta Hebert"
+        },
+        {
+          "id": 2,
+          "name": "Sherri Rojas"
+        }
+      ],
+      "greeting": "Hello, Fischer Horton! You have 5 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5c73b23e29634b31fa103e45",
+      "index": 3,
+      "guid": "3bd8fcb7-7b1b-4545-b930-56dc99595b3b",
+      "isActive": true,
+      "balance": "$1,182.06",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Travis Monroe",
+      "gender": "male",
+      "company": "AQUASURE",
+      "email": "travismonroe@aquasure.com",
+      "phone": "+1 (984) 491-2755",
+      "address": "535 Thomas Street, Townsend, Guam, 4980",
+      "about": "Aute ea qui consectetur anim Lorem ad deserunt. Fugiat laboris sit reprehenderit irure do esse aute incididunt consectetur non minim laboris ullamco anim. Voluptate deserunt do cupidatat elit laborum laborum cupidatat proident aliqua officia esse dolor. Dolor elit laborum cupidatat esse nulla aliquip et tempor pariatur ex ut. Elit officia magna cillum ad irure anim ipsum ad voluptate nostrud. Anim do labore deserunt qui est sint adipisicing aliquip pariatur deserunt.\r\n",
+      "registered": "2014-05-27T07:35:11 -06:-30",
+      "latitude": -77.124321,
+      "longitude": 35.821806,
+      "tags": [
+        "ex",
+        "irure",
+        "ea",
+        "elit",
+        "ut",
+        "quis",
+        "occaecat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Sarah Frye"
+        },
+        {
+          "id": 1,
+          "name": "Fay Jimenez"
+        },
+        {
+          "id": 2,
+          "name": "Parrish Bolton"
+        }
+      ],
+      "greeting": "Hello, Travis Monroe! You have 4 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5c73b23e2b497b9a9d10802b",
+      "index": 4,
+      "guid": "a755c458-ce1c-42a5-ac6a-e463b6a8f09c",
+      "isActive": false,
+      "balance": "$2,899.23",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Ivy Harding",
+      "gender": "female",
+      "company": "LEXICONDO",
+      "email": "ivyharding@lexicondo.com",
+      "phone": "+1 (841) 459-2631",
+      "address": "565 Coventry Road, Kidder, Maryland, 2368",
+      "about": "Aliquip non minim sunt dolore qui commodo pariatur. Laboris mollit occaecat culpa in duis velit tempor ipsum fugiat aliqua officia. Do deserunt id ut voluptate aute ullamco ad aliquip. Veniam officia officia officia ullamco amet irure incididunt officia. Aute ut laborum nostrud dolore consequat adipisicing cillum ad nostrud dolore nostrud tempor culpa minim. Exercitation nostrud magna id anim ex pariatur voluptate exercitation. Quis aliqua laborum incididunt proident minim ullamco ipsum culpa aliqua nisi culpa.\r\n",
+      "registered": "2016-04-04T10:45:36 -06:-30",
+      "latitude": 6.040503,
+      "longitude": -158.441308,
+      "tags": [
+        "minim",
+        "consequat",
+        "officia",
+        "commodo",
+        "ea",
+        "anim",
+        "amet"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lana Mcguire"
+        },
+        {
+          "id": 1,
+          "name": "Vicki Mack"
+        },
+        {
+          "id": 2,
+          "name": "Janice Evans"
+        }
+      ],
+      "greeting": "Hello, Ivy Harding! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    }
+  ];
+
+  var mappedData = myData.map(function(x,i){
+      return {userAge:x.age, userGender:x.gender, userName:x.name};
+  }).filter(function(y,o){
+      return y.userAge > 20;
+  })
+
+  console.log(mappedData);
